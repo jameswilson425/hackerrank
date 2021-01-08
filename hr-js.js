8d233ab07b3b8fe9ee31982a32de92c2e07a721e
jimmy <jimewilson87@gmail.com>
2021-01-07 22:36:40 -0600

    whiteboard

--- hr-js.js
+++ hr-js.js
@@ -56,7 +56,7 @@ console.log(bonAppetit([3, 10, 2, 9], 1, 7));
 
 
 // write conditional that says if n divided by 2 is less than p start from the back of the book
-  //if n divided by 2 is more than p, start from the front of the book
+//if n divided by 2 is more than p, start from the front of the book
 // when starting at back of book, if total number of pages is even, first subract 1 and add 1 to a page counter, then subtract 2 and add 1 to page counter until you get to the page number
 // when starting at the front of the book, add 2 to the index and add 1 to the counter until you get to the page number
 //print counter
