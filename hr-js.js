8b2f03fc6be8416815470a434837d9ca60389b8c
jimmy <jimewilson87@gmail.com>
2021-01-09 16:03:13 -0600

    whiteboard

--- hr-js.js
+++ hr-js.js
@@ -94,3 +94,29 @@ console.log(pageCount(6, 3));
 
 // INCOMPLETE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+// Loop and print
+// set sea level variable to keep track of above or below zero
+// loop through and add or subract one based off of D or U
+//
+function countingValleys(steps, path) {
+  var i = 0;
+  var seaLevel = 0;
+  var valleyCounter = 0;
+  while (i < path.length - 1) {
+    if (i === 0 && path[i] === “D”) {
+      console.log(`valley counter ${valleyCounter += 1}`);
+    }
+    else if (seaLevel === 0 && path[i + 1] === “D”) {
+      console.log(`valley counter ${valleyCounter += 1}`);
+    }
+    if (path[i] === “D”) {
+      console.log(`sea level counter in D ${seaLevel -= 1}`);
+    }
+    else if (path[i] === “U”) {
+      console.log(`sea level counter in U ${seaLevel += 1}`);
+    }
+    i += 1;
+  }
+  return valleyCounter;
+}
+console.log(countingValleys(8, “UDDDUDUU”));
