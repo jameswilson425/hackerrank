20c7ce6899eca33428479ea2e74b2af200b3240f
jimmy <jimewilson87@gmail.com>
2021-01-09 15:58:18 -0600

    whiteboard

--- hr-ruby.rb
+++ hr-ruby.rb
@@ -125,3 +125,29 @@ def birthday(s, d, m)
 end
 
 birthday([1, 2, 1, 3, 2], 3, 2)
+
+# STEPS
+# AM
+#   If AM use string[-2] to pop off the last two characters in the string -- so its military AM
+#     - Inside ^this conditional, have another if conditional that the 12 at the beggining of the string to 00
+# PM
+#   If PM, use string[-2] to pop off the last two characters in the string -- so its military PM
+#     - Inside ^this conditional, if the hours are less than 12, add 1 to string[0] and 2 to string[1]
+def time_conversion(s)
+  if s[-2] == "A"
+    s.delete! "AM"
+    if s[0] == "1" && s[1] == "2"
+      s[0] = "0"
+      s[1] = "0"
+    end
+  elsif s[-2] == "P"
+    s.delete! "PM"
+    if s.split(":", 2)[0] != "12"
+      hours = s.split(":", 2)[0].to_i + 12
+      s = hours.to_s + ":" + s.split(":", 2)[1]
+    end
+  end
+  p s
+end
+
+time_conversion("06:05:45PM")
