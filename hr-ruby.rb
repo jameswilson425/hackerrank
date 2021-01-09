cdfed87e1e43b878f90530cef5ea67f205587ab7
jimmy <jimewilson87@gmail.com>
2021-01-09 15:59:22 -0600

    whiteboard

--- hr-ruby.rb
+++ hr-ruby.rb
@@ -190,3 +190,27 @@ def divisibleSumPairs(n, k, ar)
 end
 
 divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2])
+
+# Loop through array and print integers and set up counters
+# Write conditionals to add to counters for each bird id
+# Write conditionals to comparing most seen bird to each other bird id
+# If most seen is greater, do nothing
+# If most seen is smaller, set bird id being compared to most seen variable
+# If most seen is equal, do nothing
+# print index
+def migratoryBirds(arr)
+  id = 1
+  type = 0
+  perma_count = 0
+  while id < 6
+    count = arr.count(id)
+    if count > perma_count
+      type = id
+      perma_count = count
+    end
+    id += 1
+  end
+  p type
+end
+
+migratoryBirds([1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4])
