381864df7818aadd8fc05130a650e99a23038f3f
jimmy <jimewilson87@gmail.com>
2021-01-19 12:42:16 -0600

    whiteboard WIP

--- hr-js.js
+++ hr-js.js
@@ -92,8 +92,6 @@ function pageCount(n, p) {
 console.log(pageCount(6, 3));
 
 
-// INCOMPLETE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 // Loop and print
 // set sea level variable to keep track of above or below zero
 // loop through and add or subract one based off of D or U
@@ -102,21 +100,27 @@ function countingValleys(steps, path) {
   var i = 0;
   var seaLevel = 0;
   var valleyCounter = 0;
-  while (i < path.length - 1) {
-    if (i === 0 && path[i] === “D”) {
-      console.log(`valley counter ${valleyCounter += 1}`);
-    }
-    else if (seaLevel === 0 && path[i + 1] === “D”) {
-      console.log(`valley counter ${valleyCounter += 1}`);
-    }
+  if (path[i] === “D”) {
+    console.log(`valley counter ${valleyCounter += 1}`);
+    console.log(`sea level counter ${seaLevel -= 1}`);
+    i += 1;
+  }
+  else if (path[i] === “U”) {
+    console.log(`sea level counter ${seaLevel += 1}`);
+    i += 1;
+  }
+  while (i < steps) {
     if (path[i] === “D”) {
       console.log(`sea level counter in D ${seaLevel -= 1}`);
     }
     else if (path[i] === “U”) {
       console.log(`sea level counter in U ${seaLevel += 1}`);
     }
+    if (seaLevel === 0 && path[i + 1] === “D”) {
+      console.log(`valley counter ${valleyCounter += 1}`);
+    }
     i += 1;
   }
   return valleyCounter;
 }
-console.log(countingValleys(8, “UDDDUDUU”));
+console.log(countingValleys(100, “UDUDDUUUDUDUDUUDUUDDDDDUDUDDDDUUDDUDDUUUUDUUDUDDDDUDUDUUUDDDUUUDUDDUUDDDUUDDUDDDUDUUDUUDUUDUDDDUUUUU”));
