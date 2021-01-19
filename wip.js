717dbf521787d733b9cbb3ce8705d2bdc0012734
jimmy <jimewilson87@gmail.com>
2021-01-19 11:52:12 -0600

    whiteboard WIP

--- wip.js
+++ wip.js
@@ -26,4 +26,18 @@ function countingValleys(steps, path) {
 console.log(countingValleys(8, “UDDDUDUU”));
 
 
+// STACK OVERFLOW SOLUTION
+
+function countingValleys(n, s) {
+  let elevation = 0;
+  let traversedValleys = 0;
+  for (let i = 0; i < n; i++) {
+      if (s[i] === "D") {
+          --elevation;
+      } else if (s[i] === "U") {
+          if (++elevation === 0) traversedValleys++;
+      }
+  }
+  return traversedValleys;
+}
 
