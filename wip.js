7f8044aacaa8ea3484b3ee22900b78976f8c89b4
jimmy <jimewilson87@gmail.com>
2021-01-21 13:05:33 -0600

    whiteboard trello

--- wip.js
+++ wip.js
@@ -13,3 +13,44 @@
 //   return traversedValleys;
 // }
 
+// var longestCommonPrefix = function(strs) {
+//   let prefix = ""
+//   if(strs === null || strs.length === 0) return prefix
+
+//   for (let i=0; i < strs[0].length; i++){ 
+//       const char = strs[0][i] // loop through all characters of the very first string. 
+
+//       for (let j = 1; j < strs.length; j++){ 
+//         // loop through all other strings in the array
+//           if(strs[j][i] !== char) return prefix
+//       }
+//       prefix = prefix + char
+//   }
+
+//   return prefix
+// };
+
+// console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+
+var longestCommonPrefix = function(strs) {
+  var prefix = "";
+  var i = 0;
+
+  while (i < strs[0].length) {
+    var characters = strs[0][i];
+    var i2 = 1;
+
+    while (i2 < strs.length) {
+      if (strs[i2][i] !== characters) {
+        return prefix;
+      }
+      i2 += 1;
+    }
+    prefix = prefix + characters;
+    i += 1;
+  }
+  return prefix;
+
+};
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
